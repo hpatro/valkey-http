f5db76e1b1e7d44022ba522f447753b2b1a713e3
Harkrishn Patro <bunty.hari@gmail.com>
2024-08-18 10:43:25 -0700

    Set up routes for ping and health

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use rouille::input::HttpAuthCredentials;
+use rouille::websocket;
 use rouille::Request;
 use serde::{Serialize, Deserialize};
 use std::ptr::NonNull;
 use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 use rouille::try_or_400;
 use rouille::router;
 use rouille::Response;
@@ -57,9 +60,44 @@ fn start_http_handler() {
             (DELETE) (/item/{key: String}) => {
                 process_command(&cred.login, format!("DEL {}", key))
             },
+            (GET) (/ping) => {
+                Response::text("pong")
+            },
+            (GET) (/health) => {
+                // This is the websockets route.
+
+                // In order to start using websockets we call `websocket::start`.
+                // The function returns an error if the client didn't request websockets, in which
+                // case we return an error 400 to the client thanks to the `try_or_400!` macro.
+                //
+                // The function returns a response to send back as part of the `start_server`
+                // function, and a `websocket` variable of type `Receiver<Websocket>`.
+                // Once the response has been sent back to the client, the `Receiver` will be
+                // filled by rouille with a `Websocket` object representing the websocket.
+                println!("{:?}", request.headers());
+                let (response, websocket) = try_or_400!(websocket::start(request, Some("echo")));
+
+                // Because of the nature of I/O in Rust, we need to spawn a separate thread for
+                // each websocket.
+                thread::spawn(move || {
+                    // This line will block until the `response` above has been returned.
+                    let ws = websocket.recv().unwrap();
+                    // We use a separate function for better readability.
+                    websocket_handling_thread(ws);
+                });
+                response
+            },
             _ => Response::empty_404()
         )
     });
+}
+
+fn websocket_handling_thread(mut websocket: websocket::Websocket) {
+    // We wait for a new message to come from the websocket.
+    loop {
+        websocket.send_text("PING").unwrap();
+        sleep(Duration::from_secs(1));
+    }
 }
 
 fn verify_auth(request: &Request) -> Result<HttpAuthCredentials, &'static str> {
